3568494d8e8185de367f50800c7b27c86069a63b
Komron Aripov <karipov@protonmail.com>
2024-02-03 17:11:54 -0500

    time randomizer lol

--- src/app.py
+++ src/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, jsonify
 from arduino import get_data
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/data')
 def get_data():
     data = {
-        'water_level': 0.5,
-        'humidity': 0.6,
-        'temperature': 25.5,
+        'water_level': 0.5 + (datetime.now().second / 60.0 / 4),
+        'humidity': 0.6 - (datetime.now().second / 60.0 / 4),
+        'temperature': 25.5 + (datetime.now().second),
         'co2': 0.7,
         'light': 'on',
     }

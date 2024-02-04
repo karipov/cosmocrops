bda4a36fff4e08ea2b52197606acbfe79e4697bc
Komron Aripov <karipov@protonmail.com>
2024-02-04 02:59:44 -0500

    pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'flask',
+            script: 'app.py',
+            cwd: '/home/komron/cosmocrops/src/',
+            interpreter: '/usr/bin/python3',
+        }
+    ]
+};
